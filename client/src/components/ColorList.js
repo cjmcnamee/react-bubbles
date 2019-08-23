92ec6807a6e30e463a34f8257d33a927cc1753c1
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-08-23 12:50:00 -0400

    mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth"
 
 const initialColor = {
   color: "",
@@ -21,10 +22,27 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res);
+        setColorToEdit(initialColor)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    document.location.reload(true);
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+        .then(res => {
+          this.props.history.push('/protected')
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    document.location.reload(true);
   };
 
   return (
