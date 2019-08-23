39de25da059f4a681b681dc41ab04c02c50bc3bc
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-08-23 12:03:29 -0400

    colors displayed

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -14,6 +14,7 @@ const BubblePage = () => {
     axiosWithAuth().get('http://localhost:5000/api/colors')
       .then(res => {
         console.log(res);
+        setColorList(res.data);
       })
       .catch(err => {
         console.log(err);
